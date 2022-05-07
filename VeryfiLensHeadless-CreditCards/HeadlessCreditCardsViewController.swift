32a2e8a4570808cad7dd42f399969fb2e0a388fe
Sebastián Giraldo <giraldosebastian99@gmail.com>
2022-05-06 19:57:33 -0500

    Updated view controller logic

--- VeryfiLensHeadless-CreditCards/HeadlessCreditCardsViewController.swift
+++ VeryfiLensHeadless-CreditCards/HeadlessCreditCardsViewController.swift
@@ -19,16 +19,23 @@ class HeadlessCreditCardsViewController: UIViewController {
     @IBOutlet weak var cardHolder: UILabel!
     @IBOutlet weak var guide: UILabel!
     @IBOutlet weak var guideLabel: UILabel!
+    @IBOutlet weak var flipCardImageView: UIImageView!
     
     private var isLensInitialized: Bool = false
     private var isTorchOn = false
+    private var shouldCaptureFrames = true
+    private var flipExtractionRequired = false
+    private var creditCard: CreditCard? = nil
     
-    private enum State {
-        case front
-        case back
-        case result
+    override var shouldAutorotate: Bool {
+        return false;
     }
     
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         let credentials = VeryfiLensHeadlessCredentials(clientId: "XXXXXX",
@@ -37,7 +44,8 @@ class HeadlessCreditCardsViewController: UIViewController {
                                                         url: "XXXXXX")
         
         let settings = VeryfiLensCreditCardsSettings()
-        settings.isGPUEnabled = true
+        settings.marginTop = 70
+        settings.marginBottom = 30
         
         VeryfiLensHeadless.shared().delegate = self
         
@@ -58,6 +66,7 @@ class HeadlessCreditCardsViewController: UIViewController {
         }
         
         setTorchImage()
+        cleanLabels()
         cameraView.configure()
         cameraView.delegate = self
         borderView.layer.borderColor = UIColor.white.cgColor
@@ -73,9 +82,7 @@ class HeadlessCreditCardsViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         cameraView.stopSession()
-        creditCard = nil
-        VeryfiLensHeadless.shared().reset()
-        state = .front
+        cleanUI()
         super.viewWillDisappear(animated)
     }
     
@@ -86,14 +93,6 @@ class HeadlessCreditCardsViewController: UIViewController {
         }
     }
     
-    override var shouldAutorotate: Bool {
-        return false;
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .portrait
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         if let dest = segue.destination as? HeadlessCreditCardResultViewControlerViewController {
@@ -101,116 +100,9 @@ class HeadlessCreditCardsViewController: UIViewController {
         }
     }
     
-    private var state: State = .front {
-        didSet {
-            VeryfiLensHeadless.shared().reset()
-            switch state {
-            case .front:
-                guideLabel.text = "Position your debit or credit card in the frame to scan in"
-            case .back:
-                guideLabel.text = "Flip the card over"
-            case .result:
-                isTorchOn = false
-                setTorchImage()
-                cameraView.stopSession()
-                VeryfiLensHeadless.shared().reset()
-                self.isLensInitialized = false
-                performSegue(withIdentifier: "creditCardResult", sender: self)
-            }
-        }
-    }
-    
-    var creditCard: CreditCard? = nil {
-        didSet {
-            if let value = creditCard?.holder,
-               value != "" {
-                cardHolder.isHidden = false
-                cardHolder.text = value
-            } else {
-                cardHolder.isHidden = true
-            }
-            if let value = creditCard?.number,
-               value != ""  {
-                cardNumber.isHidden = false
-                cardNumber.text = value
-            } else {
-                cardNumber.isHidden = true
-            }
-            if let value = creditCard?.dateString,
-               value != ""  {
-                cardDate.isHidden = false
-                cardDate.text = value
-            } else {
-                cardDate.isHidden = true
-            }
-            if let value = creditCard?.cvc,
-               value != ""  {
-                cardCVC.isHidden = false
-                cardCVC.text = value
-            } else {
-                cardCVC.isHidden = true
-            }
-        }
-    }
-    
-    private func setTorchImage() {
-        let imageName = isTorchOn ? "bolt.fill" : "bolt.slash.fill"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: imageName)?.resize(to: CGSize(width: 22, height: 22)), style: .plain, target: self, action: #selector(changeTorchMode))
-    }
-    
-    private func nextState() {
-        switch state {
-        case .front:
-            state = .back
-        case .back:
-            if let cvv = creditCard?.cvc,
-               cvv != "" {
-                state = .result
-            } else {
-                VeryfiLensHeadless.shared().reset()
-            }
-        case .result:
-            state = .front
-        }
-    }
-    
-    private func merge(_ creditCard1: CreditCard?, creditCard2: CreditCard?, shouldFixExistingFields: Bool) -> CreditCard? {
-        guard let creditCard1 = creditCard1 else {
-            if let creditCard2 = creditCard2 {
-                return creditCard2
-            }
-            return nil
-        }
-        guard let creditCard2 = creditCard2 else {
-            return creditCard1
-        }
-        if shouldFixExistingFields {
-            var mergedCreditCard = CreditCard()
-            mergedCreditCard.holder = (creditCard1.holder == nil ||  creditCard1.holder == "") ? creditCard2.holder ?? "" : creditCard1.holder
-            mergedCreditCard.number = (creditCard1.number == nil ||  creditCard1.number == "") ? creditCard2.number ?? "" : creditCard1.number
-            mergedCreditCard.dates = (creditCard1.dateString == "") ? creditCard2.dates ?? [] : creditCard1.dates
-            mergedCreditCard.identifier = (creditCard1.identifier == nil ||  creditCard1.identifier == "")  ? creditCard2.identifier ?? "" : creditCard1.identifier
-            mergedCreditCard.type = (creditCard1.type == nil ||  creditCard1.type == "") ? creditCard2.type ?? "" : creditCard1.type
-            mergedCreditCard.cvc = (creditCard1.cvc == nil ||  creditCard1.cvc == "") ? creditCard2.cvc ?? "" : creditCard1.cvc
-            return mergedCreditCard
-        } else {
-            var mergedCreditCard = CreditCard()
-            mergedCreditCard.holder = (creditCard2.holder != nil && creditCard2.holder != "") ? creditCard2.holder! : creditCard1.holder
-            mergedCreditCard.number = (creditCard2.number != nil && creditCard2.number != "") ? creditCard2.number! : creditCard1.number
-            mergedCreditCard.dates = (creditCard2.dateString != "") ? creditCard2.dates : creditCard1.dates
-            mergedCreditCard.identifier = (creditCard2.identifier != nil && creditCard2.identifier != "") ? creditCard2.identifier! : creditCard1.identifier
-            mergedCreditCard.type = (creditCard2.type != nil && creditCard2.type != "") ? creditCard2.type! : creditCard1.type
-            mergedCreditCard.cvc = (creditCard2.cvc != nil && creditCard2.cvc != "") ? creditCard2.cvc! : creditCard1.cvc
-            return mergedCreditCard
-        }
-    }
-    
+    //MARK: IBActions
     @IBAction func unwindToCreditCard(_ unwindSegue: UIStoryboardSegue) {
-        cameraView.startSession()
-        self.isLensInitialized = true
-        creditCard = nil
-        VeryfiLensHeadless.shared().reset()
-        state = .front
+        cleanUI()
     }
     
     @IBAction func changeTorchMode(_ sender: Any) {
@@ -236,6 +128,118 @@ class HeadlessCreditCardsViewController: UIViewController {
             }
         }
     }
+    
+    //MARK: Private
+    private func openSuccessViewController() {
+        isTorchOn = false
+        setTorchImage()
+        cameraView.stopSession()
+        VeryfiLensHeadless.shared().reset()
+        shouldCaptureFrames = false
+        flipExtractionRequired = false
+        performSegue(withIdentifier: "creditCardResult", sender: self)
+    }
+    
+    private func cleanLabels() {
+        cardCVC.text = ""
+        cardDate.text = ""
+        cardNumber.text = ""
+        cardHolder.text = ""
+    }
+        
+    private func updatePlaceholders(_ creditCard: CreditCard?, shouldOverwriteData: Bool) {
+        if let value = creditCard?.holder,
+           value != "", (cardHolder.text?.isEmpty ?? true || shouldOverwriteData) {
+            cardHolder.isHidden = false
+            cardHolder.text = value
+        } else {
+            cardHolder.isHidden = cardHolder.text?.isEmpty ?? true
+        }
+        if let value = creditCard?.number,
+           value != "", (cardNumber.text?.isEmpty ?? true || shouldOverwriteData)  {
+            cardNumber.isHidden = false
+            cardNumber.text = value
+        } else {
+            cardNumber.isHidden = cardNumber.text?.isEmpty ?? true
+        }
+        if let value = creditCard?.dateString,
+           value != "", (cardDate.text?.isEmpty ?? true || shouldOverwriteData)  {
+            cardDate.isHidden = false
+            cardDate.text = value
+        } else {
+            cardDate.isHidden = cardDate.text?.isEmpty ?? true
+        }
+        if let value = creditCard?.cvc,
+           value != "", (cardCVC.text?.isEmpty ?? true || shouldOverwriteData)  {
+            cardCVC.isHidden = false
+            cardCVC.text = value
+        } else {
+            cardCVC.isHidden = cardCVC.text?.isEmpty ?? true
+        }
+    }
+    
+    private func setTorchImage() {
+        let imageName = isTorchOn ? "bolt.fill" : "bolt.slash.fill"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: imageName)?.resize(to: CGSize(width: 22, height: 22)), style: .plain, target: self, action: #selector(changeTorchMode))
+    }
+    
+    private func updateModel(_ newCreditCard: CreditCard?) {
+        if creditCard == nil {
+            creditCard = CreditCard()
+        }
+        if creditCard?.holder?.isEmpty ?? true {
+            creditCard?.holder = newCreditCard?.holder
+            VeryfiLensHeadless.shared().settings.detectCardHolderName = creditCard?.holder?.isEmpty ?? true
+        }
+        if creditCard?.number?.isEmpty ?? true {
+            creditCard?.number = newCreditCard?.number
+            VeryfiLensHeadless.shared().settings.detectCardNumber = creditCard?.number?.isEmpty ?? true
+        }
+        if creditCard?.dates?.isEmpty ?? true {
+            creditCard?.dates = newCreditCard?.dates
+            VeryfiLensHeadless.shared().settings.detectCardDate = creditCard?.dates?.isEmpty ?? true
+        }
+        if creditCard?.identifier?.isEmpty ?? true {
+            creditCard?.identifier = newCreditCard?.identifier
+        }
+        if creditCard?.type?.isEmpty ?? true {
+            creditCard?.type = newCreditCard?.type
+        }
+        if creditCard?.cvc?.isEmpty ?? true {
+            creditCard?.cvc = newCreditCard?.cvc
+            VeryfiLensHeadless.shared().settings.detectCardCVC = creditCard?.cvc?.isEmpty ?? true
+        }
+    }
+    
+    private func animateFlipImageView() {
+        UIView.transition(with: self.flipCardImageView, duration: 1, options: [.transitionFlipFromRight], animations: {
+        }) { _ in
+            if self.flipExtractionRequired {
+                self.animateFlipImageView()
+            }
+        }
+    }
+    
+    private func requiredCardDataCompleted() -> Bool {
+        return !(creditCard?.cvc?.isEmpty ?? true) && !(creditCard?.number?.isEmpty ?? true)  && !(creditCard?.dates?.isEmpty ?? true)  && !(creditCard?.holder?.isEmpty ?? true) && !(creditCard?.dateString.isEmpty ?? true)
+    }
+    
+    
+    
+    private func cleanUI() {
+        flipCardImageView.isHidden = true
+        guideLabel.text = "Position your debit or credit card in the frame to scan in"
+        cameraView.startSession()
+        shouldCaptureFrames = true
+        creditCard = nil
+        cleanLabels()
+        flipExtractionRequired = false
+        VeryfiLensHeadless.shared().settings.detectCardNumber = true
+        VeryfiLensHeadless.shared().settings.detectCardHolderName = true
+        VeryfiLensHeadless.shared().settings.detectCardDate = true
+        VeryfiLensHeadless.shared().settings.detectCardCVC = true
+        VeryfiLensHeadless.shared().reset()
+    }
 }
 
 extension HeadlessCreditCardsViewController: CameraViewDelegate {
@@ -250,7 +254,7 @@ extension HeadlessCreditCardsViewController: CameraViewDelegate {
     }
     
     func cameraView(_ cameraView: CameraView, didCapture frame: CMSampleBuffer) {
-        if isLensInitialized {
+        if isLensInitialized && shouldCaptureFrames{
             VeryfiLensHeadless.shared().process(buffer: frame)
         }
     }
@@ -278,9 +282,7 @@ extension HeadlessCreditCardsViewController: VeryfiLensHeadlessDelegate {
         
         if let data = try? JSONSerialization.data(withJSONObject: json as Any, options: .prettyPrinted),
            let dataModel = try? decoder.decode(CreditCard.self, from: data)  {
-            if state != .back {
-                creditCard = merge(creditCard, creditCard2: dataModel, shouldFixExistingFields: false)
-            }
+            updatePlaceholders(dataModel, shouldOverwriteData: false)
         }
     }
     
@@ -290,8 +292,23 @@ extension HeadlessCreditCardsViewController: VeryfiLensHeadlessDelegate {
         
         if let data = try? JSONSerialization.data(withJSONObject: json as Any, options: .prettyPrinted),
            let dataModel = try? decoder.decode(CreditCard.self, from: data)  {
-            creditCard = merge(creditCard, creditCard2: dataModel, shouldFixExistingFields: true)
-            nextState()
+            updateModel(dataModel)
+            updatePlaceholders(creditCard, shouldOverwriteData: true)
+            if flipExtractionRequired || requiredCardDataCompleted() {
+                openSuccessViewController()
+            } else {
+                flipExtractionRequired = true
+                shouldCaptureFrames = false
+                guideLabel.text = "Flip the card over"
+                flipCardImageView.isHidden = false
+                animateFlipImageView()
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    self.flipCardImageView.isHidden = true
+                    VeryfiLensHeadless.shared().reset()
+                    self.shouldCaptureFrames = true
+                }
+            }
         }
     }
 }
